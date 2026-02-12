86dadc95be9fbd917924097209dace06a61b6b7f
adjidiopcom43 <adjidiopcom43@gmail.com>
2026-02-12 16:29:39 +0000

    Update EXO.py

--- EXO.py
+++ EXO.py
@@ -1,65 +1,25 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 2,
-   "id": "14b63f53-ffb8-4537-aea2-0c613a1c3862",
-   "metadata": {},
-   "outputs": [
-    {
-     "name": "stdin",
-     "output_type": "stream",
-     "text": [
-      "sasir note1 : 23\n",
-      "sasir note2 : 34\n"
-     ]
-    },
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
-      " la moyenne est de 57\n"
-     ]
-    }
-   ],
-   "source": [
-    "#Écrire un programme qui saisit deux notes d’un étudiant calcul et affiche la moyenne des notes.\n",
-    "\n",
-    "Not1 = int(input(\"sasir note1 :\"))\n",
-    "Not2 = int(input(\"sasir note2 :\"))\n",
-    "moyenne = Not1 + Not2\n",
-    "\n",
-    "print(\" la moyenne est de\",moyenne)"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "528e9c19-eaed-4c05-9513-aa44ff79af0e",
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3 (ipykernel)",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.13.5"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+import streamlit as st
+
+st.title(" Calculateur d'IMC")
+
+poids = st.number_input("Poids (kg) " )
+taille_cm = st.number_input("Taille (cm) :", min_value=50.0, value=170.0, step=0.1)
+taille = taille_cm / 100  # Conversion en mètres
+
+if st.button("Calculer l'IMC"):
+    if taille > 0:
+        imc = poids / (taille  2)
+        st.success(f"Votre IMC est : {imc:.2f}")
+
+        if imc < 18.5:
+            categorie = "🔴 Maigreur"
+        elif imc < 25:
+            categorie = "🟢 Corpulence normale"
+        elif imc < 30:
+            categorie = "🟡 Surpoids"
+        else:
+            categorie = "🔴 Obésité"
+
+        st.info(f"**Catégorie : {categorie}")
+    else:
+        st.error("La taille doit être supérieure à 0 !")
